bdef59bb2aa5e9c243705d6f73751a105e94fd94
Anuragkolhe <128228032+Anuragkolhe@users.noreply.github.com>
2023-04-28 13:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your JS code here. If required.
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
